1e6219e027d94fad8cad27089cf6a7fcf9869143
Frederick Johnson <fritzj96@gmail.com>
2020-01-11 22:30:41 -0800

    added: authReducer

---
+++ src/redux/reducers/auth.js
@@ -0,0 +1,19 @@
+import * as actions from '../actions/auth';
+
+const initialState = { authenticated: false, token: null, loggingIn: false, error: null }
+
+export const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.LOGIN_REQUEST:
+      return Object.assign({}, state, { loggingIn: true });
+
+    case actions.LOGIN_SUCCESS:
+      return Object.assign({}, state, { loggingIn: false, token: action.token, authenticated: true });
+
+    case actions.LOGIN_FAILURE:
+      return Object.assign({}, state, { loggingIn: false, token: null, authenticated: false, error: 'Login Error' });
+
+    default:
+      return state;
+  }
+}

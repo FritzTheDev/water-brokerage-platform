3f2e0b8dd58198cc82f0f6edfc384dadc2486e5d
Frederick Johnson <fritzj96@gmail.com>
2020-01-12 12:22:50 -0800

    changed: authenticated to isAuthenticated in state.

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -1,6 +1,6 @@
 import * as actions from '../actions/auth';
 
-const initialState = { authenticated: false, token: null, loggingIn: false, error: null }
+const initialState = { isAuthenticated: false, token: null, isVerifying: false, loggingIn: false, error: null }
 
 export const authReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -8,19 +8,19 @@ export const authReducer = (state = initialState, action) => {
       return Object.assign({}, state, { loggingIn: true });
 
     case actions.LOGIN_SUCCESS:
-      return Object.assign({}, state, { loggingIn: false, token: action.token, authenticated: true, error: null });
+      return Object.assign({}, state, { loggingIn: false, token: action.token, isAuthenticated: true, error: null });
 
     case actions.LOGIN_FAILURE:
-      return Object.assign({}, state, { loggingIn: false, token: null, authenticated: false, error: 'Login Error' });
+      return Object.assign({}, state, { loggingIn: false, token: null, isAuthenticated: false, error: 'Login Error' });
 
     case actions.LOGOUT:
-      return Object.assign({}, state, { token: null, authenticated: false, error: null });
+      return Object.assign({}, state, { token: null, isAuthenticated: false, error: null });
 
     case actions.VERIFY_REQUEST:
-      return Object.assign({}, state, { loggingIn: true });
+      return Object.assign({}, state, { isVerifying: true });
 
     case actions.VERIFY_SUCCESS:
-      return Object.assign({}, state, { loggingIn: false, token: action.token, authenticated: true, error: null });
+      return Object.assign({}, state, { isVerifying: false, token: action.token, isAuthenticated: true, error: null });
     default:
       return state;
   }

0bf46c537194310cf2343af2cb63efeff3e560dd
Frederick Johnson <fritzj96@gmail.com>
2020-01-12 16:41:58 -0800

    added: Login route

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import { IonSplitPane, IonRouterOutlet } from "@ionic/react";
 import { PrivateRoute } from "./PrivateRoute";
 import { connect } from "react-redux";
 import { ListingsPage } from "../pages/Listings";
+import { Route } from "react-router-dom";
 
 const App = ({ isAuthenticated, isVerifying }) => {
   return (
     <IonSplitPane contentId="main">
       <IonRouterOutlet id="main">
+        <Route path="/login" component={LoginPage} />
         <PrivateRoute
           isAuthenticated={isAuthenticated}
           isVerifying={isVerifying}
